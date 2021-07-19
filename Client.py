a2d3d7cd22a20bbb2685fc050b98896d0c9c6fde
Knight1632 <adityadasch@gmail.com>
2021-07-19 20:58:59 +0530

    Client commt 1

--- Client.py
+++ Client.py
@@ -1,4 +1,12 @@
 from socket import *
+from pickle import *
+import RGameObject as rgo
 class Client:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,gamename) -> None:
+        self.client=socket()
+        self.client.connect(('Vittala',5507))
+        self.close=False
+    def send(self):
+        while not self.close:
+            self.client.send(rgo.RGameObject.game)
+    def recv(self):pass
---
+++ RGameObject.py
@@ -0,0 +1,5 @@
+from pyunity import *
+class GameObj_1:pass
+class RGameObject:
+    def __init__(self):
+        self.list=[(GameObj_1,Vector3(0,0,0))]
