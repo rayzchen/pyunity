d5fd107df740d4d9282d9c1942de644f3418b8e8
Knight1632 <adityadasch@gmail.com>
2021-08-10 10:08:18 +0530

    Basic loading system

--- prefab.py
+++ prefab.py
@@ -3,14 +3,11 @@ import json
 class Test:
     def __init__(self) -> None:
         self.script='123456.py'
-        self.mesh='12.31,2532'
+        self.mesh='(12,32,63,92),(12,96,31,35),(215,513,25,1.32),(15,31,2135,2)'
         self.transform='Vector3(16,20,30)'
         self.path='D:/pyunity'
         self.name='Test'
 class Prefab:
-    asset_path=None
-    def __init__(self,path) -> None:
-        Prefab.asset_path=path
     @staticmethod
     def Genrate(go:Test):
         script=go.script
@@ -27,4 +24,9 @@ class Prefab:
         c_json=json.dumps(json_)
         with open(f'{go.name}.json','w') as json_file:
             json_file.write(c_json)
+    @staticmethod
+    def Load(go:Test):
+        with open(f'{go.name}.json','r') as json_f:
+            data=json.load(json_f)
 Prefab.Genrate(Test())
+Prefab.Load(Test())
