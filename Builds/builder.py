22d6f16ab5789a800d709a3680fd796734bd6c7f
Knight1632 <knightplayzmc1632@gmail.com>
2021-08-23 22:51:38 +0530

    Design adding commit

--- Builds/builder.py
+++ Builds/builder.py
@@ -1,44 +1,59 @@
+import json
 from tkinter import *
 from tkinter import filedialog
-from os import *
+import os
 import shutil
+from json import *
 from PIL import Image
-cd=getcwd()
+cd=os.getcwd()
 print(cd)
-chdir('./Builds')
+os.chdir('./Builds')
 root = Tk()
 root.geometry('500x200')
 root.title("Build your game")
 class Builder:
     #Init for the class
     def __init__(self) -> None:
-        self.Spath=StringVar(value=f'{path.join(cd,"icon.png")}')
+        #Path variables
+        self.Spath=StringVar(value=f'{os.path.join(cd,"icon.png")}')
         self.Epath=Entry(width=60,textvariable=self.Spath).place(x=50,y=10)
         self.Lpath=Label(text='Path:').place(x=30,y=10)
+        #Button variables
         self.b=Button(root,text='Done, build the game',command=self.build)
         self.b2=Button(root,text='Browse',command=self.browse)
         self.b.place(x=250,y=60)
         self.b2.place(x=100,y=60)
+
+        #JSON importer
+        self.json_file=open('project_data.json','r')
+        self.json=json.load(self.json_file)
+        #Design
+        self.text=Text()
+        print(self.genrate_text())
+        #self.text.insert(END,f'{self.genrate_text()}')
     #Builds/converts the game into .exe file
     def build(self):
-        for i,dirname,f in walk('.'):
+        for i,dirname,f in os.walk('.'):
             for dirnames in dirname:
                 if dirnames=='Compile':
                     shutil.rmtree('./Compile')
-        mkdir('./Compile')
+        os.mkdir('./Compile')
         shutil.copy2('test.pyw','./Compile')
         path=self.Spath.get()
         img=Image.open(r'{}'.format(path))
-        chdir('./Compile')
+        os.chdir('./Compile')
         img.save('icon.ico')
 
-        system(f'pyinstaller --onefile test.pyw --icon=icon.ico')
+        os.system(f'pyinstaller --onefile test.pyw --icon=icon.ico')
     #Lets you browse the icon for exe file
     def browse(self):
         try:
             read=filedialog.askopenfile(filetypes=(('PNG(.png)','*.png'),('JPEG(.jpeg)','*.jpeg'),('Icon(.ico)','*.ico')))
             self.Spath.set(read.name)
         except AttributeError:pass
+    #It returns text to display in Text box
+    def genrate_text(self):
+        return f'Name:{self.json["name"]}\nId:{self.json["id"]}\nEngine:{self.json["Engine"]}\n'
 Builder()
 root.wm_iconbitmap('Icon.ico')
 root.mainloop()
